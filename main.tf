07d8cb97cfe60455985c9706c27f6ac93495dbc6
mkphoenix <kareemmomoh@gmail.com>
2025-08-25 21:19:08 +0100

    initial commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "artifact_bucket" {
+resource "aws_s3_bucket" "artifact_mk_bucket" {
   bucket = "mk101-bucket-artifacts"
 
   tags = {
