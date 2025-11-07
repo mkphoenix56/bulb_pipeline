6e498ddad957dad047ae027272a72264da254c75
mkphoenix <kareemmomoh@gmail.com>
2025-11-07 23:58:12 +0100

    added new files

--- backend_pipeline.tf
+++ backend_pipeline.tf
@@ -172,7 +172,7 @@ resource "aws_codepipeline" "phoenix_backend_pipeline" {
         Owner          = "mkphoenix56"
         Repo           = "farm-stack-backend"
         Branch         = "main"
-        OAuthToken     = "data.aws_ssm_parameter.phoenix_github_token.value"
+        OAuthToken     = data.aws_ssm_parameter.phoenix_github_token.value
       }
     }
   }
